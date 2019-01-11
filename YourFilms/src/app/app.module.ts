d5505de01c5733ddd00a345e28119c887bfe751f
ekzGuille <guillermosesesantos@hotmail.com>
2019-01-11 19:02:22 +0100

    modulos añadidos

--- YourFilms/src/app/app.module.ts
+++ YourFilms/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIModule } from './ui/ui.module';
+import { FilmsModule } from './films/films.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    UIModule,
+    FilmsModule,
+    AccountsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
