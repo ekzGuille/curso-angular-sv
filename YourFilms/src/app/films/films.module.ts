995b0261c40cd80bf562968d1d6415566e48193b
ekzGuille <guillermosesesantos@hotmail.com>
2019-01-11 20:46:28 +0100

    componentes film creados

--- YourFilms/src/app/films/films.module.ts
+++ YourFilms/src/app/films/films.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
+import { SingleComponent } from './single/single.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [IndexComponent, SingleComponent, ViewComponent],
   imports: [
     CommonModule
   ]
